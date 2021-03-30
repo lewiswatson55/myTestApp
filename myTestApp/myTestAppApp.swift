86a0bfee17fd4b4e65dae78add3b32ee71721dc7
Lewis Watson <lewiswatson55@protonmail.com>
2021-03-30 04:10:57 -0700

    Initial Commit

---
+++ myTestApp/myTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myTestAppApp.swift
+//  myTestApp
+//
+//  Created by Lewis Watson on 30/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct myTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
