3b7dbb52e1c4c95734f44d9be7f7404e718dbde5
Lewis Watson <lewiswatson55@protonmail.com>
2021-03-31 15:02:06 -0700

    Added some icons and added alert button test

--- myTestApp/ContentView.swift
+++ myTestApp/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertShown = false
+    
     var body: some View {
         
         VStack {
@@ -19,30 +22,36 @@ struct ContentView: View {
                 .offset(y: -165)
                 .padding(.bottom, -200)
             
-            VStack(alignment: .leading) {
-                
-                Text("Edinburgh Castle")
-                    .font(.title)
-                HStack {
-                    Text("City of, Edinburgh")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("Scotland, United Kingdom")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                Divider()
+            ScrollView {
+                VStack(alignment: .leading) {
+                    
+                    Text("Edinburgh Castle")
+                        .font(.title)
+                    HStack {
+                        Text("City of, Edinburgh")
+                            .font(.subheadline)
+                        Spacer()
+                        Text("Scotland, United Kingdom")
+                            .font(.subheadline)
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    Divider()
 
-                Text("About Edinburgh Castle")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
+                    Text("About Edinburgh Castle")
+                        .font(.title2)
+                    Text("Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...Descriptive text goes here. This is a test to see if xcode is going to crash again whilst typing into the program... so far so good...")
+                }
+                    
+                    .padding()
+                    Spacer()
             }
-                
-                .padding()
-                Spacer()
+            Button("Show Alert") {
+                self.alertShown = true
+            }.alert(isPresented: $alertShown) { () -> Alert in
+                Alert(title: Text("Alert Title"), message: Text("Alert Message"), dismissButton: .default(Text("Ok")))
+            }
         }
-        Text("Other")
     }
 }
 
